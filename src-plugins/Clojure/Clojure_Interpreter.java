51e352195784f9591314bc1d9af0257df76e3236
Albert Cardona <cardona@ucla.edu>
2008-04-25 15:35:38 +0200

    Recompiled clojure.jar for java 1.5.0

--- src-plugins/Clojure/Clojure_Interpreter.java
+++ src-plugins/Clojure/Clojure_Interpreter.java
@@ -208,7 +208,7 @@ public class Clojure_Interpreter extends AbstractInterpreter {
 							sb.append('\n');
 						};
 						// remove last newline char
-						sb.setLength(sb.length()-1);
+						if (sb.length() > 0) sb.setLength(sb.length()-1);
 
 					} catch (Throwable t) {
 						error = t;
