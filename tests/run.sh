bab9f1c736c7b807ac7388814aae3190b16c5948
Albert Cardona <cardona@ucla.edu>
2008-05-13 13:27:49 -0700

    Added -Dplugins.dir to tests/run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -5,4 +5,4 @@
 # Run all jython tests contained in this current folder
 #
 CLASSPATH=.:../ij.jar:$(find ../jars/ -name "*.jar" -exec printf ":%s" {} \;)$(find ../plugins/ -name "*.jar" -exec printf ":%s" {} \;)
-java -Dpython.home="../jars/jython21" -classpath $CLASSPATH org.python.util.jython *.py
+java -Dpython.home="../jars/jython21" -Dplugins.dir=".." -classpath $CLASSPATH org.python.util.jython *.py
