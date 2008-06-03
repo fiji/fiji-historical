9db6e8917235a8c324dac7cda7e48f922c17c306
Johannes Schindelin <johannes.schindelin@gmx.de>
2008-06-03 11:11:07 +0100

    Fake: implement globbing
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- fake/Fake.java
+++ fake/Fake.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,6 +12,8 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
+
+import java.util.regex.Pattern;
 
 public class Fake {
 	protected static boolean debug = true;
@@ -110,12 +113,80 @@ public class Fake {
 		allRules.put(rule.target, rule);
 	}
 
+	static class GlobFilter implements FilenameFilter {
+		Pattern pattern;
+
+		GlobFilter(String glob) {
+			String regex = "^" + glob.replace(".", "\\.")
+				.replace("^", "\\^").replace("$", "\\$")
+				.replace("?", ".").replace("*", ".*") + "$";
+			pattern = Pattern.compile(regex);
+		}
+
+		public boolean accept(File dir, String name) {
+			return pattern.matcher(name).matches();
+		}
+	}
+
+	protected static int expandGlob(String glob, List list)
+			throws FakeException {
+		if (glob.indexOf("**") >= 0)
+			throw new FakeException("Unsupported glob: " + glob);
+
+		// find first wildcard
+		int star = glob.indexOf('*'), qmark = glob.indexOf('?');
+
+		// no wildcard?
+		if (star < 0 && qmark < 0) {
+			list.add(glob);
+			return 1;
+		}
+
+		if (qmark >= 0 && qmark < star)
+			star = qmark;
+
+		int prevSlash = glob.lastIndexOf('/', star);
+		int nextSlash = glob.indexOf('/', star);
+
+		String parentPath = prevSlash < 0 ?
+			new File("").getAbsolutePath() :
+			glob.substring(0, prevSlash);
+		File parentDirectory = new File(parentPath);
+		if (!parentDirectory.exists())
+			throw new FakeException("Directory '" + parentDirectory
+				+ "' not found");
+
+		String pattern = glob.substring(prevSlash + 1, nextSlash < 0 ?
+			glob.length() : nextSlash);
+
+		String remainder = nextSlash < 0 ?
+			null : glob.substring(nextSlash);
+
+		String[] names = parentDirectory.list(new GlobFilter(pattern));
+
+		parentPath = prevSlash < 0 ? "" : parentPath + "/";
+		int count = nextSlash < 0 ? names.length : 0;
+		for (int i = 0; i < names.length; i++)
+			if (nextSlash < 0)
+				list.add(parentPath + names[i]);
+			else if (new File(parentPath + names[i]).isDirectory())
+				count += expandGlob(parentPath + names[i]
+						+ remainder, list);
+
+		return count;
+	}
+
 	public static Rule addRule(String target, String prerequisites)
 			throws FakeException {
 		List list = new ArrayList();
 		StringTokenizer tokenizer = new StringTokenizer(prerequisites);
-		while (tokenizer.hasMoreTokens())
-			list.add(tokenizer.nextToken());
+
+		while (tokenizer.hasMoreTokens()) {
+			String token = tokenizer.nextToken();
+			if (expandGlob(token, list) == 0)
+				throw new FakeException("Glob did not match any"
+					+ " file: '" + token + "'");
+		}
 
 		Rule rule = null;
 
