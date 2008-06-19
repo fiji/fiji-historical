8d9b85d7d590e206024828d07ed0a972757a28f4
Johannes Schindelin <johannes.schindelin@gmx.de>
2008-06-19 16:51:34 +0100

    Fake.sh: also check for missing fake.jar
    While at it, also potentially fix it for Windows.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- Fake.sh
+++ Fake.sh
@@ -2,20 +2,25 @@
 
 CWD="$(dirname "$0")"
 
-test "a$@" != afake.jar -a "$CWD"/fake/Fake.java -nt "$CWD"/fake.jar &&
-sh "$0" fake.jar
-
-test -f "$CWD"/fiji &&
-exec "$CWD"/fiji --fake "$@"
-
 case "$(uname -s)" in
-Darwin) platform=macosx;;
-Linux) platform=linux;;
-MINGW*|CYGWIN*) platform=win32;;
+Darwin) platform=macosx; exe=;;
+Linux) platform=linux; exe=;;
+MINGW*|CYGWIN*) platform=win32; exe=.exe;;
 esac
 
-test -f "$CWD"/precompiled/fiji-$platform &&
-exec "$CWD"/precompiled/fiji-$platform --fake -- "$@"
+test "a$@" != afake.jar &&
+test ! -f "$CWD"/fake.jar -o "$CWD"/fake/Fake.java -nt "$CWD"/fake.jar &&
+sh "$0" fake.jar
+
+test "a$@" != afake.jar -a "a$@" != "fiji" &&
+test ! -f "$CWD"/fiji -o "$CWD"/fiji.cxx -nt "$CWD"/fiji$exe &&
+sh "$0" fiji
+
+test -f "$CWD"/fiji$exe &&
+exec "$CWD"/fiji$exe --fake "$@"
+
+test -f "$CWD"/precompiled/fiji-$platform$exe &&
+exec "$CWD"/precompiled/fiji-$platform$exe --fake -- "$@"
 
 test -f "$CWD"/fake.jar &&
 java -classpath "$CWD"/fake.jar Fake "$@"
