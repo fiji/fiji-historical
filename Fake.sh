2b050ed13ec4f6f937bc02b62046c40105bfeb51
Johannes Schindelin <johannes.schindelin@gmx.de>
2008-06-19 05:09:52 -0700

    Add Fake.sh, a convenience helper to start Fiji mAKE
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ Fake.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+CWD="$(dirname "$0")"
+test -f "$CWD"/fiji &&
+exec "$CWD"/fiji --fake "$@"
+
+case "$(uname -s)" in
+Darwin) platform=macosx;;
+Linux) platform=linux;;
+MINGW*|CYGWIN*) platform=win32;;
+esac
+
+test -f "$CWD"/precompiled/fiji-$platform &&
+exec "$CWD"/precompiled/fiji-$platform --fake -- "$@"
+
+test -f "$CWD"/fake.jar &&
+java -classpath "$CWD"/fake.jar Fake "$@"
+
+test -f "$CWD"/precompiled/fake.jar &&
+java -classpath "$CWD"/Ãprecompiled/fake.jar Fake "$@"
+
+javac "$CWD"/fake/Fake.java &&
+java -classpath "$CWD"/fake Fake "$@"
