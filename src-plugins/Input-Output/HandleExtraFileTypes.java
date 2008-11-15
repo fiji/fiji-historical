72bd6121935361c94059d29f0bc9b7e747834b0a
Johannes Schindelin <johannes.schindelin@gmx.de>
2008-11-15 21:17:13 +0100

    Add an ICO_Reader to open .ico files
    Thanks to image4j (http://image4j.sourceforge.net), this plugin was
    written pretty quickly.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/Input-Output/HandleExtraFileTypes.java
+++ src-plugins/Input-Output/HandleExtraFileTypes.java
@@ -243,6 +243,10 @@ public class HandleExtraFileTypes extends ImagePlus implements PlugIn {
 			return tryPlugIn("io.PDF_Viewer", path);
 		}
 
+		// Johannes Schindelin: open one or more images in a .ico file
+		if (name.endsWith(".ico"))
+			return tryPlugIn("io.ICO_Reader", path);
+
 		// Johannes Schindelin: handle scripts
 		if (name.endsWith(".py"))
 			return tryPlugIn("Jython.Refresh_Jython_Scripts", path);
