fc110c07504a133de21c0e67778ca78b838c821f
Johannes Schindelin <johannes.schindelin@gmx.de>
2008-11-20 20:14:44 +0100

    Add an initial version of a LiveCD creator script
    This script is able to generate a live CD on any recent Ubuntu/Intel
    system.  It does so by creating a chroot'ed environment, and using the
    fantastic bootcd package.  A few things are still to do:
    
    	- generate /etc/passwd and friends, instead of reusing the
    	  current systems' (spilling secrets),
    	- use a different user than the current one,
    	- install X11, Java, and an appropriate window manager,
    	- copy Fiji over,
    	- configure it so the desktop starts without logging in, and
    	  start Fiji right away.
    
    Also: this works only with i386 so far, need to investigate how to
    support 64-bit (x86_64, _not_ ia64).
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ scripts/make-livecd.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+# TODO: make use of user "fiji"
+
+NAME=gutsy
+CHROOT=chroot-$NAME-i386
+CHROOT_HOME=/home/$USER
+DCHROOT="dchroot -c $NAME"
+
+test -d $CHROOT || {
+	mkdir $CHROOT &&
+	sudo apt-get install -q -y --force-yes dchroot debootstrap &&
+	sudo debootstrap --arch i386 $NAME \
+		./$CHROOT/ http://archive.ubuntu.com/ubuntu &&
+	for i in passwd shadow group sudoers hosts
+	do
+		sudo cp /etc/$i ./$CHROOT/etc/
+	done &&
+
+	(grep "$(pwd)/$CHROOT" /etc/dchroot.conf 2> /dev/null ||
+	 sudo sh -c "echo \"$NAME $(pwd)/$CHROOT\" >> /etc/dchroot.conf") &&
+
+	sudo mkdir $CHROOT/$CHROOT_HOME &&
+	sudo chown $USER.$USER $CHROOT/$CHROOT_HOME || {
+		echo "Could not make chroot"
+		exit 1
+	}
+}
+
+APTSOURCE="deb http://ftp.de.debian.org/debian etch main"
+APTSOURCEFILE=/etc/apt/sources.list
+grep "$APTSOURCE" $CHROOT$APTSOURCEFILE > /dev/null 2> /dev/null || {
+	sudo sh -c "echo $APTSOURCE >> $CHROOT$APTSOURCEFILE"
+	$DCHROOT "sudo apt-get update" ||
+	exit
+}
+
+APTSOURCE="deb http://de.archive.ubuntu.com/ubuntu/ gutsy main restricted"
+APTSOURCEFILE=/etc/apt/sources.list
+grep "$APTSOURCE" $CHROOT$APTSOURCEFILE > /dev/null 2> /dev/null || {
+	sudo sh -c "echo $APTSOURCE >> $CHROOT$APTSOURCEFILE"
+	$DCHROOT "sudo apt-get update" ||
+	exit
+}
+
+test -f $CHROOT/usr/bin/bootcdwrite ||
+$DCHROOT "sudo apt-get install -q -y --force-yes bootcd" ||
+exit
+
+test -f $CHROOT/vmlinuz ||
+$DCHROOT "sudo apt-get install -q -y --force-yes linux-image" ||
+exit
+
+# TODO: install X11 and Java
+
+$DCHROOT "sudo bash bootcdwrite -s"
+
+ISO=/var/spool/bootcd/cdimage.iso
+$DCHROOT "sudo chmod a+w $ISO"
+
+mv $CHROOT$ISO fiji-livecd.iso
