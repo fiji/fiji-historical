b473d68f07be032305bfdd0df4996cb94445179a
Yap Chin Kiet <leoforever05@gmail.com>
2009-06-17 23:08:57 +0800

    RecordsBuilderUI able to display information
    Used artificial list of plugins to do that.

--- src-plugins/Plugin_Manager/src/fiji/pluginManager/RecordsBuilderUI.java
+++ src-plugins/Plugin_Manager/src/fiji/pluginManager/RecordsBuilderUI.java
@@ -5,10 +5,13 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -16,7 +19,10 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.ListModel;
 import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class RecordsBuilderUI extends JFrame {
 	private PluginManager pluginManager;
@@ -30,10 +36,87 @@ public class RecordsBuilderUI extends JFrame {
 	private JButton btnUpload;
 	private JButton btnClose;
 
+	private List<PluginObject> pluginRecords;
+
 	public RecordsBuilderUI(PluginManager pluginManager) {
 		this.pluginManager = pluginManager;
 		setupUserInterface();
 		pack();
+
+		pluginRecords = new PluginCollection();
+		Dependency dependencyA1 = new Dependency("PluginD.jar", "20090420190033");
+		ArrayList<Dependency> Adependency = new ArrayList<Dependency>();
+		Adependency.add(dependencyA1);
+		PluginObject pluginA = new PluginObject("PluginA.jar", "65c3ecc1bbd7564f92545ffd2521f9d96509ca64", "20090429190842", null, Adependency, PluginObject.STATUS_UNINSTALLED, PluginObject.ACTION_NONE);
+
+		Dependency dependencyB1 = new Dependency("PluginD.jar", "20090420190033");
+		Dependency dependencyB2 = new Dependency("PluginH.jar", "20090524666220");
+		Dependency dependencyB3 = new Dependency("PluginC.jar", "20081011183621");
+		ArrayList<Dependency> Bdependency = new ArrayList<Dependency>();
+		Bdependency.add(dependencyB1);
+		Bdependency.add(dependencyB2);
+		Bdependency.add(dependencyB3);
+		PluginObject pluginB = new PluginObject("PluginB.jar", "9624fa93cbf7720c01c7ff97c28b00747b700de3", "20090429190854", null, Bdependency, PluginObject.STATUS_UNINSTALLED, PluginObject.ACTION_NONE);
+
+		PluginObject pluginC = new PluginObject("PluginC.jar", "1a992dbc077ef84020d44a980c7992ba6c8edf3d", "20090425190854", null, null, PluginObject.STATUS_UNINSTALLED, PluginObject.ACTION_NONE);
+
+		Dependency dependencyD1 = new Dependency("PluginF.jar", "20090420191023");
+		Dependency dependencyD3 = new Dependency("PluginE.jar", "20090311213621");
+		ArrayList<Dependency> Ddependency = new ArrayList<Dependency>();
+		Ddependency.add(dependencyD1);
+		Ddependency.add(dependencyD3);
+		PluginObject pluginD = new PluginObject("PluginD.jar", "61c3ecc1add7364f92545ffd2521e9d96508cb62", "20090420190033", null, Ddependency, PluginObject.STATUS_UNINSTALLED, PluginObject.ACTION_NONE);
+
+		Dependency dependencyE1 = new Dependency("PluginG.jar", "20090125190842");
+		ArrayList<Dependency> Edependency = new ArrayList<Dependency>();
+		Edependency.add(dependencyE1);
+		PluginObject pluginE = new PluginObject("PluginE.jar", "8114fe93cbf7720c01c7ff97c28b007b79900dc7", "20090311213621", null, Edependency, PluginObject.STATUS_UNINSTALLED, PluginObject.ACTION_NONE);
+
+		Dependency dependencyF1 = new Dependency("PluginI.jar", "20090501190854");
+		ArrayList<Dependency> Fdependency = new ArrayList<Dependency>();
+		Fdependency.add(dependencyF1);
+		PluginObject pluginF = new PluginObject("PluginF.jar", "1b992dbca07ef84020d44a980c7902ba6c82dfee", "20090420191023", null, Fdependency, PluginObject.STATUS_UNINSTALLED, PluginObject.ACTION_NONE);
+		
+		PluginObject pluginG = new PluginObject("PluginG.jar", "1a992dbc077ef84020d44a980c7992ba6c8edf3d", "20090415160854", null, null, PluginObject.STATUS_UNINSTALLED, PluginObject.ACTION_NONE);
+
+		Dependency dependencyH1 = new Dependency("PluginD.jar", "20090420190033");
+		ArrayList<Dependency> HnewDependency = new ArrayList<Dependency>();
+		HnewDependency.add(dependencyH1);
+		PluginObject pluginH = new PluginObject("PluginH.jar", "33c88dc1fbd7564f92587ffdc521f9de6507ca65", "20081224666220", null, null, PluginObject.STATUS_UNINSTALLED, PluginObject.ACTION_NONE);
+
+		Dependency dependencyI1 = new Dependency("PluginF.jar", "20090420191023");
+		Dependency dependencyI2 = new Dependency("PluginK.jar", "20081221866291");
+		ArrayList<Dependency> Idependency = new ArrayList<Dependency>();
+		Idependency.add(dependencyI1);
+		Idependency.add(dependencyI2);
+		PluginObject pluginI = new PluginObject("PluginI.jar", "9624fa93cbf7720c01c7ff97c28b00747b700de3", "20090429190854", null, Idependency, PluginObject.STATUS_UNINSTALLED, PluginObject.ACTION_NONE);
+
+		Dependency dependencyJ1 = new Dependency("PluginI.jar", "20090404090854");
+		ArrayList<Dependency> Jdependency = new ArrayList<Dependency>();
+		Jdependency.add(dependencyJ1);
+		PluginObject pluginJ = new PluginObject("PluginJ.jar", "9624fa93cbf7720c01c7ff97c28b00747b700de3", "20090521181954", null, Jdependency, PluginObject.STATUS_UNINSTALLED, PluginObject.ACTION_NONE);
+
+		Dependency dependencyK1 = new Dependency("PluginJ.jar", "20090404090854");
+		ArrayList<Dependency> Kdependency = new ArrayList<Dependency>();
+		Kdependency.add(dependencyK1);
+		PluginObject pluginK = new PluginObject("PluginK.jar", "9624fa93cbf7720c01c7ff97c28b00747b700de3", "20081221866291", null, Kdependency, PluginObject.STATUS_UNINSTALLED, PluginObject.ACTION_NONE);
+
+		pluginRecords.add(pluginA);
+		pluginRecords.add(pluginB);
+		pluginRecords.add(pluginC);
+		pluginRecords.add(pluginD);
+		pluginRecords.add(pluginE);
+		pluginRecords.add(pluginF);
+		pluginRecords.add(pluginG);
+		pluginRecords.add(pluginH);
+		pluginRecords.add(pluginI);
+		pluginRecords.add(pluginJ);
+		pluginRecords.add(pluginK);
+
+		DefaultListModel listModel = (DefaultListModel)listLocalPlugins.getModel();
+		for (PluginObject plugin : pluginRecords) {
+			listModel.addElement(plugin.getFilename());
+		}
 	}
 
 	private void setupUserInterface() {
@@ -47,10 +130,47 @@ public class RecordsBuilderUI extends JFrame {
 		lblPanel.add(Box.createHorizontalGlue());
 
 		listLocalPlugins = new JList();
+		listLocalPlugins.setModel(new DefaultListModel());
+		listLocalPlugins.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		listLocalPlugins.setPreferredSize(new Dimension(270, 370));
 		JScrollPane listScrollpane = new JScrollPane(listLocalPlugins);
 		listScrollpane.getViewport().setBackground(listLocalPlugins.getBackground());
 		listScrollpane.setPreferredSize(new Dimension(300, 370));
+		listLocalPlugins.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+
+			//Called when a row is selected
+			public void valueChanged(ListSelectionEvent event) {
+				String selectedName = (String)listLocalPlugins.getSelectedValue();
+				if (selectedName == null) {
+					//Selection got filtered away
+				} else {
+					for (PluginObject plugin : pluginRecords) {
+						if (plugin.getFilename().equals(selectedName)) {
+							lblFilename.setText("Filename: " + plugin.getFilename());
+							lblDigest.setText("Md5 sum: " + plugin.getmd5Sum());
+							lblDate.setText("Timestamp: " + plugin.getTimestamp());
+							lblFilesize.setText("Filesize: " + plugin.getFilesize());
+							List<Dependency> dependencyList = plugin.getDependencies();
+							DefaultListModel listModel = new DefaultListModel();
+							if (dependencyList != null) {
+								for (Dependency dependency : dependencyList) {
+									listModel.addElement(dependency.getFilename() + "; " + dependency.getTimestamp());
+								}
+							} else if (dependencyList == null || dependencyList.size() == 0) {
+								listModel.addElement("None.");
+							}
+							listDependencies.setModel(listModel);
+							//ignore dependencies first
+							if (plugin.getDescription() != null)
+								txtDescription.setText(plugin.getDescription());
+							break;
+						}
+					}
+					//private JList listDependencies;
+				}
+			}
+
+		});
 
 		JPanel leftPanel = new JPanel();
 		leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
@@ -122,10 +242,10 @@ public class RecordsBuilderUI extends JFrame {
 		rightPanel.add(lblDependenciesTitle, c);
 
 		listDependencies = new JList();
-		listDependencies.setPreferredSize(new Dimension(400, 120));
+		listDependencies.setPreferredSize(new Dimension(400, 80));
 		JScrollPane dependenciesScrollpane = new JScrollPane(listDependencies);
 		dependenciesScrollpane.getViewport().setBackground(listDependencies.getBackground());
-		dependenciesScrollpane.setPreferredSize(new Dimension(430, 120));
+		dependenciesScrollpane.setPreferredSize(new Dimension(430, 80));
 		c = new GridBagConstraints();
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 0;
@@ -145,6 +265,7 @@ public class RecordsBuilderUI extends JFrame {
 		c.weighty = 0.5;
 		rightPanel.add(lblDescriptionTitle, c);
 
+		//Only editing of description is allowed, the rest are generated automatically
 		txtDescription = new JTextArea();
 		txtDescription.setPreferredSize(new Dimension(400, 180));
 		JScrollPane txtScrollpane = new JScrollPane(txtDescription);
