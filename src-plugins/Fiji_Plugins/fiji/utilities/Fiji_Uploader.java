dd6074e8e405109ca84d00fdc3364f9ef8a24bd5
Yap Chin Kiet <leoforever05@gmail.com>
2009-07-14 18:57:37 +0800

    Refactored Fiji_Uploader for uploading file
    Fiji_Uploader.java of Fiji_Plugins/ now relies on a "logic" class
    FileUploader.java (Separation of UI from logic). NOTE: The new
    logic class FileUploader.java still assumes only a single file
    is uploaded. Multiple file support TODO.

--- src-plugins/Fiji_Plugins/fiji/utilities/Fiji_Uploader.java
+++ src-plugins/Fiji_Plugins/fiji/utilities/Fiji_Uploader.java
@@ -7,27 +7,15 @@ package fiji.utilities;
  * Author: Johannes E. Schindelin
  */
 
-import com.jcraft.jsch.Channel;
-import com.jcraft.jsch.ChannelExec;
-import com.jcraft.jsch.HostKey;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.Session;
-
-import com.jcraft.jsch.UserInfo;
-
+import fiji.utilities.FileUploader.UploadListener;
 import ij.IJ;
-
 import ij.io.OpenDialog;
-
 import ij.plugin.PlugIn;
-
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import com.jcraft.jsch.HostKey;
 
-public class Fiji_Uploader implements PlugIn {
+public class Fiji_Uploader implements PlugIn, UploadListener {
+	FileUploader fileUploader;
 	final String user = "uploads";
 	final String host = "pacific.mpi-cbg.de";
 	final int hostKeyType = HostKey.SSHRSA;
@@ -58,110 +46,28 @@ public class Fiji_Uploader implements PlugIn {
 			return; // canceled
 
 		File file = new File(od.getDirectory(), od.getFileName());
+		fileUploader = new FileUploader(host, hostKey, hostKeyType);
 		try {
-			JSch jsch = new JSch();
-			HostKey hostKey = new HostKey(host, hostKeyType,
-				getHostKeyBytes());
-			jsch.getHostKeyRepository().add(hostKey, null);
-
-			Session session = jsch.getSession(user, host, 22);
-			session.setPassword(pwd);
-			session.connect();
-
-			// exec 'scp -t file' remotely
-			String path = file.getName().replace(' ', '_');
-			String command = "scp -p -t incoming/" + path;
-			Channel channel = session.openChannel("exec");
-			((ChannelExec)channel).setCommand(command);
-
-			// get I/O streams for remote scp
-			OutputStream out = channel.getOutputStream();
-			InputStream in = channel.getInputStream();
-
-			channel.connect();
-
-			if(checkAck(in) != 0)
-				return;
-
-			// send "C0644 filesize filename"
-			long filesize = file.length();
-			command = "C0444 " + filesize + " " + path + "\n";
-			out.write(command.getBytes());
-			out.flush();
-			if(checkAck(in) != 0)
-				return;
-
-			// send a content of file
-			IJ.showStatus("Uploading " + file.getName());
-			FileInputStream input = new FileInputStream(file);
-			byte[] buf = new byte[16384];
-			long total = 0;
-			for (;;) {
-				int len = input.read(buf, 0, buf.length);
-				if (len <= 0)
-					break;
-				out.write(buf, 0, len);
-				total += len;
-				IJ.showProgress(total / (float)filesize);
-			}
-			IJ.showProgress(1, 1);
-			input.close();
-
-			// send '\0'
-			buf[0] = 0;
-			out.write(buf, 0, 1);
-			out.flush();
-			if (checkAck(in) != 0)
-				return;
-			out.close();
-
-			channel.disconnect();
-			session.disconnect();
-		}
-		catch(Exception e){
+			fileUploader.connectSession(user, pwd);
+		} catch(Exception e){
 			IJ.error(e.toString());
 		}
+		fileUploader.addListener(this);
+		fileUploader.uploadFile(file);
+		fileUploader.disconnectSession();
 	}
 
-	static int checkAck(InputStream in) throws IOException{
-		int b = in.read();
-		// b may be 0 for success,
-		//          1 for error,
-		//          2 for fatal error,
-		//          -1
-		if (b == 0)
-			return b;
-		if (b == -1)
-			return b;
-
-		if (b == 1 || b == 2) {
-			StringBuffer sb = new StringBuffer();
-			int c;
-			do {
-				c = in.read();
-				sb.append((char)c);
-			}
-			while (c != '\n');
-			IJ.error(sb.toString());
-		}
-		return b;
+	public void uploadComplete(File source) {
+		IJ.showStatus("Upload successful!");
+		IJ.showProgress(1, 1);
 	}
 
-	byte[] getHostKeyBytes() {
-		byte[] result = new byte[hostKey.length() / 3];
-		for (int i = 0; i < result.length; i++)
-			result[i] = (byte)(fromHex(hostKey.charAt(i * 3 + 2))
-				| (fromHex(hostKey.charAt(i * 3 + 1)) << 4));
-		return result;
+	public void uploadFailed(File source, Exception e) {
+		IJ.error(e.toString());
 	}
 
-	int fromHex(char c) {
-		if (c >= '0' && c <= '9')
-			return c - '0';
-		if (c >= 'a' && c <= 'f')
-			return c - 'a' + 10;
-		if (c >= 'A' && c <= 'F')
-			return c - 'A' + 10;
-		throw new RuntimeException("Illegal hex character: " + c);
+	public void update(File source, int bytesSoFar, int bytesTotal) {
+		IJ.showStatus("Uploading " + source.getName());
+		IJ.showProgress(bytesSoFar, bytesTotal);
 	}
 }
--- src-plugins/Plugin_Manager/src/fiji/pluginManager/logic/Downloader.java
+++ src-plugins/Plugin_Manager/src/fiji/pluginManager/logic/Downloader.java
@@ -1,7 +1,9 @@
 package fiji.pluginManager.logic;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
@@ -60,15 +62,19 @@ public class Downloader {
 					downloadedBytes += count;
 					notifyListenersUpdate();
 				}
-
 				//end connection once download done
 				in.close();
 				out.close();
 				connection.disconnect();
 				notifyListenersCompletion();
 
-			} catch (Exception e) {
-				notifyListenersError(e);
+			} catch (FileNotFoundException e1) {
+				notifyListenersError(e1);
+			} catch (IOException e2) {
+				notifyListenersError(e2);
+			} catch (Exception e3) {
+				notifyListenersError(new Exception("Failed to download from " +
+						currentSource.getURL()));
 			}
 		}
 	}
